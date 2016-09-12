045fc5d8f8f3858f8ef725c2f7df43d5acc914c1
ktarow <ktarou569@gmail.com>
2016-09-13 08:08:40 +0900

    update centos6

--- scripts/centos6.8.sh
+++ scripts/centos6.8.sh
@@ -1,10 +1,12 @@
+cd $HOME/git/cfg
+
 virt-install \
---name centos68 \
+--name $VMNAME \
 --network=bridge:br0 \
---disk path=/vm-image/centos68.img,size=100 \
+--disk path=$VMPATH,size=100 \
 --ram 8192 \
 --vcpus 2 \
 --graphics none \
 --location http://ftp.iij.ad.jp/pub/linux/centos/6.8/os/x86_64 \
---initrd-inject=./git/cfg/centos6-ks.cfg \
+--initrd-inject=./centos6-ks.cfg \
 --extra-args "ks=file:centos6-ks.cfg console=tty0 console=ttyS0,115200"
