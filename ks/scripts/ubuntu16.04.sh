0545db42d0ca6ab682d51c4d9077c68867c638bc
ktarow <ktarou569@gmail.com>
2017-03-16 21:06:59 +0900

    update

--- ks/scripts/ubuntu16.04.sh
+++ ks/scripts/ubuntu16.04.sh
@@ -1,10 +1,10 @@
 virt-install \
---name ubuntu \
+--name $VMNAME \
 --network=bridge:br0 \
---disk path=/vm-image/test.img,size=50 \
+--disk path=$VMPATH/$VMNAME,size=50 \
 --ram 2048 \
 --vcpus 2 \
 --graphics none \
 --location http://ftp.riken.jp/Linux/ubuntu/dists/xenial/main/installer-amd64 \
---initrd-inject=./git/cfg/preseed.cfg \
+--initrd-inject=./preseed.cfg \
 --extra-args "console=tty0 console=ttyS0,115200"
