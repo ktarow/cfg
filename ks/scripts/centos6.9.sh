16949bc8b62e3fc8c36ad34c3751fce5a8826b4c
ktarow <ktarou569@gmail.com>
2017-04-24 23:37:15 +0900

    update os from 6.8 to 6.9

--- ks/scripts/centos6.9.sh
+++ ks/scripts/centos6.9.sh
@@ -7,6 +7,6 @@ virt-install \
 --ram 4096 \
 --vcpus 2 \
 --graphics none \
---location http://ftp.iij.ad.jp/pub/linux/centos/6.8/os/x86_64 \
+--location http://ftp.iij.ad.jp/pub/linux/centos/6.9/os/x86_64 \
 --initrd-inject=./centos6-ks.cfg \
 --extra-args "ks=file:centos6-ks.cfg console=tty0 console=ttyS0,115200"
